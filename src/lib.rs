4ee3bee1100f55c9703e0b29c519508b2c0db0e9
Max Seiden <max@sigmacomputing.io>
2017-05-04 14:09:37 -0700

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+extern crate hyper;
+extern crate mime;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+extern crate url;
+extern crate yup_oauth2;
+
+pub mod common;
+pub mod cloudkms1;
+pub mod datastore1;
